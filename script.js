f4dbbf88c75a4cd9f9ace4a6f06c74a73c1d13d7
Stefan Flosbach <stefanflosbach@t-online.de>
2025-10-07 17:02:43 +0200

    Kreis und Kreuz Animation erstellt

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ function render() {
 
             let symbol = '';
             if (field === 'circle') {
-                symbol = 'O';
+                symbol = generateCircleSVG();
             } else if (field === 'cross') {
-                symbol = 'X';
+                symbol = generateCrossSVG();
             }
 
             tableHTML += `<td>${symbol}</td>`;
@@ -41,3 +41,71 @@ function render() {
 
     contentDiv.innerHTML = tableHTML;
 }
+
+function generateCircleSVG() {
+    return `
+        <svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+            <circle 
+                cx="35" 
+                cy="35" 
+                r="30" 
+                stroke="#00B0EF" 
+                stroke-width="8" 
+                fill="none"
+                stroke-dasharray="188.4"
+                stroke-dashoffset="188.4"
+            >
+                <animate 
+                    attributeName="stroke-dashoffset" 
+                    from="188.4" 
+                    to="0" 
+                    dur="1s" 
+                    fill="freeze" 
+                />
+            </circle>
+        </svg>
+    `;
+}
+
+function generateCrossSVG() {
+    return `
+        <svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+            <line 
+                x1="15" y1="15" 
+                x2="55" y2="55" 
+                stroke="#FFC000" 
+                stroke-width="8" 
+                stroke-linecap="round"
+                stroke-dasharray="56.57"
+                stroke-dashoffset="56.57"
+            >
+                <animate 
+                    attributeName="stroke-dashoffset" 
+                    from="56.57" 
+                    to="0" 
+                    dur="0.6s" 
+                    fill="freeze" 
+                />
+            </line>
+            <line 
+                x1="55" y1="15" 
+                x2="15" y2="55" 
+                stroke="#FFC000" 
+                stroke-width="8" 
+                stroke-linecap="round"
+                stroke-dasharray="56.57"
+                stroke-dashoffset="56.57"
+            >
+                <animate 
+                    attributeName="stroke-dashoffset" 
+                    from="56.57" 
+                    to="0" 
+                    dur="0.6s" 
+                    fill="freeze" 
+                    begin="0.3s" 
+                />
+            </line>
+        </svg>
+    `;
+}
+
